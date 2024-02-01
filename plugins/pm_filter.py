4922428fd010aef014ddde152f9a8931c0469422
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-02-01 22:10:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                   text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                   text=f"📂[{get_size(file.file_size)}]➢{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -109,7 +109,7 @@ async def next_page(bot, query):
                     text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"📂{get_size(file.file_size)}➢",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
